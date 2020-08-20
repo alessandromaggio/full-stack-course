d06e12f40d54df84d85918aa573e5abe8bbc05bc
Alessandro Maggio <alessandro.maggio96@outlook.com>
2020-08-20 18:42:22 +0200

    JavaScript Tutorial: If-Else and How To Use It

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-var logMessage = "I am a log message for Full Stack Developers!";
-console.log(logMessage);
+let num = 10;
+
+if (num % 2 === 1) {
+  console.log("Number is odd");
+} else if (num === 0) {
+  console.log("The value is zero");
+} else {
+  console.log("Number is even");
+}
