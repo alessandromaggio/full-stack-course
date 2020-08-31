160d8ca08c72cb5633625eb6ca0fa108724ec617
Alessandro Maggio <alessandro.maggio96@outlook.com>
2020-08-31 18:51:01 +0200

    JavaScript Tutorial: Array Definitive Guide

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-let a = 0;
-let b = 1;
-while(a + b < 1000) {
-  let total = a + b;
-  console.log(a + ' + ' + b + ' = ' + total);
-  a = b;
-  b = total;
+let source = [1, 2, 13, 147, -1, 45, 23, 29, 31, 32, 37, 40];
+let primes = [];
+
+for(let i=0; i < source.length; i++) {
+  if (source[i] <= 1) continue;
+  let prime = true;
+  for (let j=2; j < source[i]; j++) {
+    if (source[i] % j === 0) {
+      prime = false;
+      break;
+    }
+  }
+  if (prime) primes.push(source[i]);
 }
+
+console.log(primes);
