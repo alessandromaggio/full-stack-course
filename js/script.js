3f2e336160cd2d727b7736faabb0083a700b4f64
Alessandro Maggio <alessandro.maggio96@outlook.com>
2020-09-12 15:04:08 +0200

    JavaScript Tutorial: Functions, the Definitive Guide

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
-let source = [1, 2, 13, 147, -1, 45, 23, 29, 31, 32, 37, 40];
-let primes = [];
-
-for(let i=0; i < source.length; i++) {
-  if (source[i] <= 1) continue;
-  let prime = true;
-  for (let j=2; j < source[i]; j++) {
-    if (source[i] % j === 0) {
-      prime = false;
-      break;
-    }
+function min(arr) {
+  let min = arr[0];
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
   }
-  if (prime) primes.push(source[i]);
+  return min;
 }
 
-console.log(primes);
+function max(arr) {
+  let max = arr[0];
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+}
+
+function avg(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+function calc(arr) {
+  if (arr.length === 0) return undefined;
+  return [
+    min(arr),
+    max(arr),
+    avg(arr)
+  ];
+}
+
+console.log(calc([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(calc([10]));
+console.log(calc([-1, -40, 23, 12]));
+console.log(calc([]));
