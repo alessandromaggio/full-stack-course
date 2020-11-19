47d519b492f8552e25c072fd88faf35e786ca361
Alessandro Maggio <alessandro.maggio96@outlook.com>
2020-11-19 19:12:14 +0100

    Vue Tutorial: How to Use Vue Slots

---
+++ src/components/AppDialog.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  name: 'AppDialog',
+  props: {
+    value: {
+      type: Boolean,
+      default: true,
+    }
+  },
+  methods: {
+    onClick(event) {
+      if (event.target.className === 'app-dialog__overlay') {
+        this.$emit('input', false);
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="app-dialog" v-show="value">
+    <div class="app-dialog__underlay"/>
+    <div class="app-dialog__overlay" @click="onClick">
+      <div class="app-dialog__vcenter">
+        <div class="app-dialog__dialog">
+          <slot/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style>
+.app-dialog__overlay,
+.app-dialog__underlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+}
+
+.app-dialog__overlay {
+  z-index: 101;
+}
+
+.app-dialog__underlay {
+  background-color: #000;
+  opacity: 0.75;
+  z-index: 100;
+}
+
+.app-dialog__vcenter {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.app-dialog__dialog {
+  padding: 1rem;
+  border-radius: 4px;
+  background-color: white;
+  display: inline-block;
+}
+</style>
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,16 +2,30 @@
   <div>
     <h2>Order #{{ $route.params.id }}</h2>
     <order-form/>
+    <button @click="dialog = true">Order</button>
+    <app-dialog v-model="dialog">
+      <h3>Confirm?</h3>
+      <p>Do you really want to confirm your order</p>
+      <button @click="dialog = false">Cancel</button>
+      <button @click="dialog = false">Confirm</button>
+    </app-dialog>
   </div>
 </template>
 
 <script>
 import OrderForm from '../components/OrderForm.vue';
+import AppDialog from '../components/AppDialog.vue';
 
 export default {
   name: 'Order',
+  data() {
+    return {
+      dialog: false,
+    };
+  },
   components: {
     OrderForm,
+    AppDialog,
   },
 }
 </script>
